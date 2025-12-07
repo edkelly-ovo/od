c435ab132d086f696e3998e9f3f4f15a589093fe
Ed Kelly <me@edkelly.co.uk>
2025-12-07 14:11:58 +0000

    Data corrected

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ async function loadAllPods() {
         'drive-home-and-exports.json', 'emo.json', 'enterprise-technology.json',
         'experience-foundations.json', 'fulfil.json', 'home-services.json',
         'infrastructure-and-enablement.json', 'iops.json', 'payments.json',
-        'payments-strategy.json', 'ptlt.json', 'release-ops.json',
+        'payments-strategy.json', 'ptlt.json', 'release-operations.json',
         'security.json', 'serve.json', 'service-operations.json'
     ];
 
